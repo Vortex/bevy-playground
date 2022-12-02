71f46d9546eaf715e54c359b47b03575d0effb40
Peco Danajlovski <vortexmk@gmail.com>
2022-12-02 22:18:02 +0100

    revert to bevy 0.8 to fix starting issues

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use bevy::prelude::*;
 
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .insert_resource(WindowDescriptor {
+            width: WIDTH,
+            height: HEIGHT,
+            title: "Bevy Playground".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
