64e1bb64cf572281ea0cecb4aa1899f5f1bb3695
Peco Danajlovski <vortexmk@gmail.com>
2022-12-02 22:32:54 +0100

    introduced plane and a cube

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::erased_serde::__private::serde::__private::de};
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1280.0;
@@ -13,6 +13,7 @@ fn main() {
             resizable: false,
             ..Default::default()
         })
+        .add_startup_system(spawn_basic_scene)
         .add_startup_system(spawn_camera)
         .add_plugins(DefaultPlugins)
         .run();
@@ -21,6 +22,24 @@ fn main() {
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
 }
