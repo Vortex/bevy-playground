607dcfecb7181cb2d344dd5c1aeab954bf0a5d63
Peco Danajlovski <vortexmk@gmail.com>
2022-12-06 00:09:30 +0100

    basic movement and updated tile size

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use player::PlayerPlugin;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
 
 fn main() {
     let height = 900.0;
--- src/player.rs
+++ src/player.rs
@@ -1,19 +1,49 @@
 use bevy::prelude::*;
 
-use crate::AsciiSheet;
+use crate::{AsciiSheet, TILE_SIZE};
 
 pub struct PlayerPlugin;
 
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player);
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement);
+    }
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::W) {
+        transform.translation.y += player.speed * TILE_SIZE * time.delta_seconds();
+    }
+
+    if keyboard.pressed(KeyCode::S) {
+        transform.translation.y -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+
+    if keyboard.pressed(KeyCode::A) {
+        transform.translation.x -= player.speed * TILE_SIZE * time.delta_seconds();
+    }
+
+    if keyboard.pressed(KeyCode::D) {
+        transform.translation.x += player.speed * TILE_SIZE * time.delta_seconds();
     }
 }
 
 fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
     let mut sprite = TextureAtlasSprite::new(1);
     sprite.color = Color::rgb(0.3, 0.3, 0.9);
-    sprite.custom_size = Some(Vec2::splat(1.0));
+    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
 
     let player = commands
         .spawn_bundle(SpriteSheetBundle {
@@ -25,12 +55,13 @@ fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
             },
             ..Default::default()
         })
-        .insert(Name::new("Playert"))
+        .insert(Name::new("Player"))
+        .insert(Player { speed: 3.0 })
         .id();
 
     let mut background_sprite = TextureAtlasSprite::new(0);
     background_sprite.color = Color::rgb(0.5, 0.5, 0.5);
-    background_sprite.custom_size = Some(Vec2::splat(1.0));
+    background_sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
 
     let background = commands
         .spawn_bundle(SpriteSheetBundle {
