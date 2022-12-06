69ac009f4cbd506b8e26dffde2fb16ebafe5719a
Peco Danajlovski <vortexmk@gmail.com>
2022-12-06 17:56:04 +0100

    moved all the tile entities under single parent

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -17,18 +17,27 @@ impl Plugin for TileMapPlugin {
 
 fn create_simple_map(mut commands: Commands, ascii: Res<AsciiSheet>) {
     let file = File::open("assets/map.txt").expect("No map file found");
+    let mut tiles = Vec::new();
 
     for (y, line) in BufReader::new(file).lines().enumerate() {
         if let Ok(line) = line {
             for (x, char) in line.chars().enumerate() {
-                spawn_ascii_sprite(
+                let tile = spawn_ascii_sprite(
                     &mut commands,
                     &ascii,
                     char as usize,
                     Color::rgb(0.9, 0.9, 0.9),
                     Vec3::new(x as f32 * TILE_SIZE, -(y as f32) * TILE_SIZE, 100.0),
                 );
+                tiles.push(tile);
             }
         }
     }
+
+    commands
+        .spawn()
+        .insert(Name::new("Map"))
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .push_children(&tiles);
 }
